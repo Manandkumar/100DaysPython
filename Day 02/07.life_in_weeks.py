86056d4be48565b8c236e6f5fa9c9fb617ade1c4
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-03-25 14:28:58 +0530

    Create 07.life_in_weeks.py

---
+++ Day 02/07.life_in_weeks.py
@@ -0,0 +1,12 @@
+#program to caluculate number of weeks left in life, it they leave for 90 years 
+
+#Take user age
+current_age= input("Please enter your age:")
+#conver the age to integer
+age=int(current_age)
+
+#caluculation
+life_span= 90-age
+weeks_left=life_span*52
+
+print("Life left for you is Weeks."{weeks_left})

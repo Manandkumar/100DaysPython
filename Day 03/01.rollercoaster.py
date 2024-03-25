6998065111894c58470b5539d63de4e560f09f39
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-03-25 19:06:18 +0530

    updates

--- Day 03/01.rollercoaster.py
+++ Day 03/01.rollercoaster.py
@@ -1,10 +1,12 @@
 print ("Welcome to the Rollercoasgter !")
 #take necessary information from the user 
 
+# = is assignment and ==  is checking 
+
 height=int(input ("Whats your height in CM: "))
 
 #condition check for the height
-if height>120:
+if height>=120:
     print(" Welcome to the Ride!! , Please proceed")
 else:
     print("Sorry! your height dosen't match to the required height!!")
---
+++ Day 03/02.oddoreven.py
@@ -0,0 +1,8 @@
+# Program to check if a number is Odd or Even
+user_number= int(input("Please enter a number of your choice: "))
+
+#logic to find if the entered number is Odd or Even
+if user_number%2==0:
+    print(f"Entered {user_number} is a Even number")
+else:
+    print(f"Entered {user_number} is a Odd number")

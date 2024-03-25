5ec1f26a98952d9e359866b1ee49fec6eb016cc1
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-03-25 18:53:13 +0530

    new updates

--- Day 02/08.tipcalculator.py
+++ Day 02/08.tipcalculator.py
@@ -3,7 +3,7 @@
 # Welcome Message 
 print("Welcome to the Tip Caluculator")
 total_amount= input("What's the total bill amount:")
-total_amount =int(total_amount)
+total_amount =float(total_amount)
 tip_choice = input("How much tip you like to give? 10, 12, or 15?")
 tip_choice = int(tip_choice)
 number_guests = input ("How many people to split the bill")
@@ -14,5 +14,4 @@ each_person = round((total_amount + (tip_choice*total_amount /100))/number_guest
 each_person =float(each_person)
 
 #print the amount
-print("The sheare of individual from group of ",number_guests,"is : ",each_person)
-
+print("The sheare of individual from group of ",number_guests,"is : $ ",each_person)
---
+++ Day 03/01.rollercoaster.py
@@ -0,0 +1,10 @@
+print ("Welcome to the Rollercoasgter !")
+#take necessary information from the user 
+
+height=int(input ("Whats your height in CM: "))
+
+#condition check for the height
+if height>120:
+    print(" Welcome to the Ride!! , Please proceed")
+else:
+    print("Sorry! your height dosen't match to the required height!!")

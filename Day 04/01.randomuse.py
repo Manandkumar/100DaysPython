3dd94401e3eaa449dd96ab68a87157e15d94e9eb
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-03-27 20:41:47 +0530

    Create 01.randomuse.py

---
+++ Day 04/01.randomuse.py
@@ -0,0 +1,9 @@
+import random
+
+#random Integer
+random_integer= random.randint(1,10)
+print(random_integer)
+
+#random() will be between 0.000000000 to 0.9999999999999999
+random_float= random.random()
+print(random_float)

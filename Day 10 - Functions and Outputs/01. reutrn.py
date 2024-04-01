722fd585e07b5a0800825159599519d0952bcbdf
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-01 13:44:04 +0530

    Create 01. reutrn.py

---
+++ Day 10 - Functions and Outputs/01. reutrn.py
@@ -0,0 +1,11 @@
+# define functions
+
+def capitalize(f_name,l_name):
+    print(f_name.title())
+    print(l_name.title())
+    
+# Welcome Message 
+
+f_name= input(str("Enter your First Name: "))
+l_name= input(str("Enter your First Name: "))
+

8152dc08dc8b384b9729dcac9df4f2816168ab81
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-03 14:29:18 +0530

    new

--- Day 10 - Functions and Outputs/01. reutrn.py
+++ Day 10 - Functions and Outputs/01. reutrn.py
@@ -1,11 +1,20 @@
 # define functions
 
 def capitalize(f_name,l_name):
-    print(f_name.title())
-    print(l_name.title())
+    if f_name =="" or l_name =="":
+        print("Didn't enter First name or Last name")
+        return
+    else:
+        formated_fname=f_name.title()
+        formated_lname=l_name.title()
+        return f"{formated_fname} {formated_lname}"
+   
+    
     
 # Welcome Message 
 
 f_name= input(str("Enter your First Name: "))
 l_name= input(str("Enter your First Name: "))
+
+print(capitalize(f_name,l_name))
 
---
+++ Day 10 - Functions and Outputs/02.DaysYearMonth.py
@@ -0,0 +1,34 @@
+
+def is_leapyear(year):
+    if year%4==0:
+        if year%100==0:
+            if year%400 ==0:
+                return 1
+                #print("It's a leap year")
+            else:
+                return 0
+                #print("It's not a leap year")
+        else:
+            return 1
+            #print("It's a leap Year")
+    else: 
+        return 0
+        #print("It's not a leap year")
+
+
+def days_in_month(year,month):
+    month_days =['31','28','31','30','31','30','31','31','30','31','30','31',]
+    chkdays = month_days[month]
+    chkyear = is_leapyear(year)
+    if chkyear == 1 and month ==2:
+        chkdays == 29
+        print(f"It's a Leap year and days are {chkdays}")
+    elif chkyear == 0:
+        chkdays = month_days[month-1]
+        print(f"It's not a Leap year and days are {chkdays}")
+
+        
+uyear=int(input("Enter the year you would like to check for Leap year: "))
+umonth=int(input("Enter the number of the month you like to check the days: "))
+
+days_in_month(uyear,umonth)

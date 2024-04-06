0ef64ee5215c0368a5dda9922024c754aef1704b
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-06 13:09:32 +0530

    ok

---
+++ Day 15 - IDE/01. Coffee.py
@@ -0,0 +1,16 @@
+# intial coffee machine - water 300 ml, mil 200 ml, coffee 100 g and Money 0
+# 3 hot flavours - Espresso[50 ml ater,18g coffee], Latte[200 ml water, 24g coffee, 150 ml milk], Cappucciono [250ml water, 24g coffee, 100 ml milk] 
+# nicle 
+# coin operate
+# automatic cup dispense 
+# counting cup seliing
+
+# Print report how mcuh of resources left
+# check the resources sufficicnet and say what's insufficent 
+# process coins 
+# check trascation succesful
+# manage resources 
+
+
+u_choice = input(int(("What would you like? press your choice : 1. Espresso/ 2. Latte/ 3. Cappuccino")))
+

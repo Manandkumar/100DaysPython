f25f727f5f697582498bb5fb259d6b90b1e00048
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-07 18:39:22 +0530

    Create 01.objectandclass.py

---
+++ Day 16 - OOP/01.objectandclass.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+#object = blueprint
+leo=Turtle()
+my_screen = Screen()
+
+#object.attributes
+my_screen.canvheight
+print(my_screen.canvheight)
+#object.functions() / Methods
+my_screen.bgcolor("Red")
+leo.shape("turtle")
+leo.color("green")
+leo.shapesize(10)
+my_screen.exitonclick()

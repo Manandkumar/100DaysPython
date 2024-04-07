7c2ab9ea733c06291f9ef1f77b77a5afd94d7329
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-07 18:03:05 +0530

    Update 01. Coffee.py

--- Day 15 - IDE/01. Coffee.py
+++ Day 15 - IDE/01. Coffee.py
@@ -10,32 +10,98 @@
 # process coins 
 # check trascation succesful
 # manage resources 
-
+MONEY =0
 WATER=300
 MILK=200
 COFFEE=100
-MONEY= 0
+
 
 #def check_resources():
     
-def user_transaction():
-    print("Step #1")
-
+def user_transaction(choice,amount):
+    print(f"Your choice of drink is {choice}, it cost {amount}, and please pay in cash")
+    global MONEY
+    change_1= int(input("Enter how many 10 Rs: "))
+    change_2= int(input("Enter how many 50 Rs: "))
+    change_3= int(input("Enter how many 100 Rs: "))
+    change_4= int(input("Enter how many 200 Rs: "))
+    total = (change_1*10)+(change_2*50)+(change_3*100)+(change_4*200)
+    if total <amount:
+        print("You gave insufficent Amount")
+    elif total > amount:
+        change_return= total-amount
+        print(f"You have given more than the actual amount, please take your change {change_return} ")
+        MONEY = MONEY +amount
+    elif total == amount:
+        print("Proceed to take your drink in a moment")
+        MONEY = MONEY +amount
+            
 def usage_report():
-    print(WATER,MILK, COFFEE)
+    print(f"Current Report \n Water: {WATER} \n Milk: {MILK} \n Coffee: {COFFEE}\n Total Amount collected: {MONEY}")
 
-coffee_continue = True
+def resources_reset():
+    global WATER, MILK, COFFEE
+    WATER +=300 
+    MILK += 200
+    COFFEE += 100
+    print(f"Report after Refilling \n Water: {WATER} \n Milk: {MILK} \n Coffee: {COFFEE}\n Total Amount collected: {MONEY}")
 
-while coffee_continue:
-    
-    u_choice = int(input(("What would you like? press your choice : 1. Espresso/ 2. Latte/ 3. Cappuccino")))
+def main_function():
+    global WATER,COFFEE,MILK,MONEY
 
-    if u_choice == 1 and WATER >= 50 and COFFEE >=18:
-        user_transaction()
-        WATER -=50
-        COFFEE -=18
-        usage_report()
-    else:
-        print("Insufficient resources")
-        usage_report()
-        coffee_continue == False
+    coffee_continue = True
+    while coffee_continue==True:
+        
+        u_choice = int(input(("What would you like? press your choice : 1. Espresso/ 2. Latte/ 3. Cappuccino: ")))
+
+        if u_choice == 1:
+            if WATER >= 50:
+                if COFFEE >=18:
+                    WATER -=50
+                    COFFEE -=18
+                    amount = 150
+                    user_transaction(u_choice,amount)
+                else:
+                    print("Coffee is insufficent")
+            else:
+                print("Water is insufficent")
+
+        elif u_choice == 2:
+            if WATER >= 200:
+                if COFFEE >=24:
+                    if MILK >=150:
+                        WATER -=200
+                        COFFEE -=24
+                        MILK -=150
+                        amount = 250
+                        user_transaction(u_choice,amount)
+                    else:
+                        print("Milk is insufficent")
+                else:
+                    print("Coffee is insufficent")
+            else:
+                print("Water is insufficent")
+        elif u_choice == 3:
+            if WATER >= 250:
+                if COFFEE >=24:
+                    if MILK >=100:
+                        WATER -=250
+                        COFFEE -=24
+                        MILK -=100
+                        amount = 100
+                        user_transaction(u_choice,amount)
+                    else:
+                        print("Milk is insufficent")
+                else:
+                    print("Coffee is insufficent")
+            else:
+                print("Water is insufficent")
+
+        elif u_choice==4:
+            usage_report()
+            exit()
+        elif u_choice==5:
+            resources_reset() 
+            
+
+main_function()

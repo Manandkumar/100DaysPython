d72a769bebd754a57af8f2bd86b67fa9f8afbf03
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-10 22:07:23 +0530

    ok

---
+++ Day 17 - Creating Classes/02. QuizGame.py
@@ -0,0 +1,16 @@
+from questionmodel import Question
+from questiondata import question_data
+from quizbrain import QuizBrain
+
+question_bank =[]
+
+for question in question_data:
+    question_text =question["text"]
+    question_answer =question["answer"]
+    new_question =Question(question_text,question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
---
+++ Day 17 - Creating Classes/questiondata.py
@@ -0,0 +1,14 @@
+question_data =[
+    {"text":"Do camels have three sets of eyelids?", "answer": "Tue"},
+    {"text":"It is possible to sneeze while sleeping", "answer": "FALSE"},
+    {"text":"Australia is a country as well as a continent.", "answer": "TRUE"},
+    {"text":"Herbivore animals consume meat", "answer": "FALSE"},
+    {"text":"The cell nucleus was discovered by Robert Brown.", "answer": "TRUE"},
+    {"text":"The national flag of America has 50 stars", "answer": "TRUE"},
+    {"text":"Pneumonia is an infection that affects lungs.", "answer": "TRUE"},
+    {"text":"The capital of Spain is Madrid", "answer": "TRUE"},
+    {"text":"Ears are responsible for smelling", "answer": "FALSE"},
+    {"text":"The color of cherries is red", "answer": "TRUE"},
+    {"text":"1. Butterflies are not birds.", "answer": "TRUE"},
+    {"text":"The capital of France is Washington DC", "answer": "FALSE"}
+]

80d1ae91b514d07a9836ac84b8f912557fc008c4
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-12 21:57:23 +0530

    Update 10.cologram.py

--- Day 18 - Graphics, Tuples, Importing Modules/10.cologram.py
+++ Day 18 - Graphics, Tuples, Importing Modules/10.cologram.py
@@ -1,3 +1,37 @@
-import colorgram
-colours = colorgram.extract('image.jpg',30)
-print(colours)
+import turtle as t
+import random
+from turtle import Screen
+
+my_leo = t.Turtle()
+t.colormode(255)
+my_screen = Screen()
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    colour = (r,g,b)
+    return colour
+
+my_leo.speed(10)
+my_leo.penup()
+my_leo.setheading(225)
+my_leo.forward(300)
+my_leo.setheading(0)
+
+number_of_dots =100
+
+
+for dot_count in range(1,number_of_dots):
+    my_leo.pendown()
+    my_leo.dot(50,random_color())
+    my_leo.penup()
+    my_leo.forward(50)  
+    if dot_count %10==0:
+        my_leo.setheading(90)
+        my_leo.forward(50)
+        my_leo.setheading(180)
+        my_leo.forward(500)
+        my_leo.setheading(0)
+
+my_screen.exitonclick()

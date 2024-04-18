d9cd34c387098aec091068159ba31e51df999ab4
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-18 19:11:30 +0530

    ok

--- Day 20 - snake game/scoreboard.py
+++ Day 20 - snake game/scoreboard.py
@@ -4,5 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score =0
-        self.color=("white")
+        self.color=("red")
         self.write(f"Score: {self.score}", align="center",font=("Arial",24, "normal"))
+        self.hideturtle()
+        self.goto(0,250)

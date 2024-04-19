2b91f8a0fb2d574b1d6d0d4d00856a37c5e21d3d
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-19 19:00:52 +0530

    ok

---
+++ Day 22 - Pong Game/01. steps.py
@@ -0,0 +1,15 @@
+#1 Create the screen
+
+#2 create and movet he paddle
+
+#3 Create another paddle
+
+#4 create the ball and mke it move 
+
+#5 create collision with wall and bounce
+
+#6 detect collision with paddle
+
+#7 detect when paddle misses
+
+#8 keep score 

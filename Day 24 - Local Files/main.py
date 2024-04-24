f2cabc7898eb30e6b34bacc139c8dccf7d604d83
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-24 13:17:55 +0530

    ok

--- Day 24 - Local Files/main.py
+++ Day 24 - Local Files/main.py
@@ -1,6 +1,11 @@
-with open("../my_file.txt",mode="a") as file :
-    contents = file.read
-    print(contents)
-    file.write("Anand Kumar ")
+from datetime import time, date
 
-file.close()
+with open("./Day 24 - Local Files/my_file.txt","a") as data:
+    local_time= date.month
+    data.write(f" Time the file is opened: {local_time} \n")
+    data.close()
+
+file1 = open("my_file.txt","r")
+text = file1.readlines()
+print(text)
+file1.close()

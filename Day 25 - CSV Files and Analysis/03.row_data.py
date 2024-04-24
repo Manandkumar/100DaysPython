fa24bc4465c7acee4d7d0990ac1408eaea3f6fff
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-24 17:24:00 +0530

    ok

--- Day 25 - CSV Files and Analysis/03.row_data.py
+++ Day 25 - CSV Files and Analysis/03.row_data.py
@@ -3,4 +3,4 @@ import pandas
 my_data = pandas.read_csv("./Day 25 - CSV Files and Analysis/weather_data.csv")
 print(my_data[my_data["Condition"]=="Rainy"])
 setMax= my_data["Temperature"].max()
-print(my_data[my_data["Temperature"]< setMax])
+print(my_data[my_data["Temperature"]==setMax])

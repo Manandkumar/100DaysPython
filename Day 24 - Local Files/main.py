489ba18e455213c3c1414b7680a69229a0d51b08
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-24 13:24:06 +0530

    ok

--- Day 24 - Local Files/main.py
+++ Day 24 - Local Files/main.py
@@ -1,11 +1,11 @@
 from datetime import time, date
 
 with open("./Day 24 - Local Files/my_file.txt","a") as data:
-    local_time= date.month
+    local_time= str(date.day)
     data.write(f" Time the file is opened: {local_time} \n")
     data.close()
 
-file1 = open("my_file.txt","r")
-text = file1.readlines()
+file1 = open("./Day 24 - Local Files/my_file.txt","r")
+text = file1.read()
 print(text)
 file1.close()

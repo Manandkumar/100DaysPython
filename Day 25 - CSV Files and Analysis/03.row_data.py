ba5a1042578798b71234138e809af3fed600cf1e
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-24 23:09:53 +0530

    new

--- Day 25 - CSV Files and Analysis/03.row_data.py
+++ Day 25 - CSV Files and Analysis/03.row_data.py
@@ -4,3 +4,14 @@ my_data = pandas.read_csv("./Day 25 - CSV Files and Analysis/weather_data.csv")
 print(my_data[my_data["Condition"]=="Rainy"])
 setMax= my_data["Temperature"].max()
 print(my_data[my_data["Temperature"]==setMax])
+
+#create a dataframe
+
+data_dict ={
+    "student": ['anand', 'mitul', 'ahaan'],
+    "scores":[76,56,86]
+}
+
+data= pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("./Day 25 - CSV Files and Analysis/new_data.csv")

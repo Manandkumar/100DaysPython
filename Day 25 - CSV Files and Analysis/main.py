10639051d454fd705b0d568b24a3d6963037c532
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-24 15:09:53 +0530

    ok

--- Day 25 - CSV Files and Analysis/main.py
+++ Day 25 - CSV Files and Analysis/main.py
@@ -1,5 +1,11 @@
 import csv
 with open("./Day 25 - CSV Files and Analysis/weather_data.csv") as my_file:
     contents= csv.reader(my_file)
+    temperature =[]
     for row in contents:
+        if row[1]!= "Tem":
+            temperature.append(int(row[1]))
         print(row)
+    print (temperature)
+
+import pandas

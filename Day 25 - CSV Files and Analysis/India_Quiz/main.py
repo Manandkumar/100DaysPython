0a55d71b209eb5507a56a530be9da91b53a8839b
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-04-27 20:34:05 +0530

    ok

--- Day 25 - CSV Files and Analysis/India_Quiz/main.py
+++ Day 25 - CSV Files and Analysis/India_Quiz/main.py
@@ -4,14 +4,31 @@ import pandas
 screen =turtle.Screen()
 screen.title ("INIDA STATES - QUIZ")
 image= "./Day 25 - CSV Files and Analysis/India_Quiz/India_state.gif"
-data = pandas.read_csv("./Day 25 - CSV Files and Analysis/India_Quiz/states.csv")
 screen.addshape(image)
-
 turtle.shape(image)
-
 answer_state= screen.textinput(title="Guess the state",prompt= "State name:")
 
-screen.exitonclick()
+data = pandas.read_csv("./Day 25 - CSV Files and Analysis/India_Quiz/states.csv")
+all_states = data.state.to_list()
+guess_states =[]
+
+while len(guess_states)<=30:
+    answer_state =screen.textinput(title="Guess the state", prompt="Ampther state name?").title()
+
+    if answer_state == "Exit":
+        break
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        # t.goto(state_data.x,state_data.y)
+        t.goto(0,0)
+       
+        t.write(answer_state)
+
+
 
 # def get_mouse_click(x,y):
 #     print (x,y)

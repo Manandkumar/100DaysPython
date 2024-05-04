9577d366b84288e0593662281f9a4811dedfc35c
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-04 10:15:57 +0530

    04.05.24

---
+++ Day 27 - TKinter/main.py
@@ -0,0 +1,23 @@
+import tkinter
+window = tkinter.Tk()
+window.title("My First GUI")
+window.minsize(width=600, height=400)
+
+#components inside the window
+
+#label 
+mylabel= tkinter.Label()
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()

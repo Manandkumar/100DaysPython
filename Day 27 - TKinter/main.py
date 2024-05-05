428034087cb3b0842c743379935a44659e3e2037
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-05 22:13:32 +0530

    Update main.py

--- Day 27 - TKinter/main.py
+++ Day 27 - TKinter/main.py
@@ -1,20 +1,26 @@
-import tkinter
-window = tkinter.Tk()
+from tkinter import *
+
+window = Tk()
 window.title("My First GUI")
 window.minsize(width=600, height=400)
 
 #components inside the window
 
 #label 
-mylabel= tkinter.Label( text ="I'm a label ", font=("Arial", 24))
+mylabel= Label( text ="I'm a label ", font=("Arial", 24))
 mylabel.pack(side="left")
 
-# Advance Arugumnets 
+# mylabel["text"] = "NEw Name -Anand"
 
-import turtle
+#button 
 
-leo= turtle.Turtle()
-leo.write("left")
+def button_click():
+    print("you pressed me ")
+
+
+button = Button(text= "Press",command=button_click)
+button.pack()
+
 
 
 

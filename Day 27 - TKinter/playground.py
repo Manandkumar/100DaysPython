3ac3afe6da6bb24c68586ebf736aea060cf0c5f5
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-05 22:01:51 +0530

    Update playground.py

--- Day 27 - TKinter/playground.py
+++ Day 27 - TKinter/playground.py
@@ -1,7 +1,25 @@
+# Advance Aruguments
 def add (*args):
+    print(args[2])
     total = 0
     for n in args:
         total = total+n
     return total
 
+def caluculate(**kwargs):
+    print(kwargs)    
+    print(kwargs["add"])
+
 print(add(1,2,3,4,5))
+
+caluculate(add =3, multiply=10 )
+
+class Car:
+    def __init__(self,**kw):
+        self.make=kw["make"]
+        self.color=kw["color"]
+
+my_car = Car(make="Skoda", color ="Red")
+print(my_car.color)
+
+

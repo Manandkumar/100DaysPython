b7965070b72b48b010d64de847835551ae12abaa
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-05 22:03:38 +0530

    new

--- Day 27 - TKinter/playground.py
+++ Day 27 - TKinter/playground.py
@@ -15,11 +15,15 @@ print(add(1,2,3,4,5))
 caluculate(add =3, multiply=10 )
 
 class Car:
+    # def __init__(self,**kw):
+    #     self.make=kw["make"]
+    #     self.color=kw["color"]
     def __init__(self,**kw):
-        self.make=kw["make"]
-        self.color=kw["color"]
+         self.make=kw.get("make")
+         self.color=kw.get("color")
 
-my_car = Car(make="Skoda", color ="Red")
+
+my_car = Car(make="Skoda")
 print(my_car.color)
 
 

04318c0b287eff91b6a08ce6fbade9572ca9524b
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-06 15:35:04 +0530

    Update main2.py

--- Day 27 - TKinter/main2.py
+++ Day 27 - TKinter/main2.py
@@ -4,13 +4,17 @@ window = Tk()
 window.title("Multiple Items")
 window.minsize(width=600,height=400)
 
+def click_button():
+    print(" You have clicked the button!")
+    my_label.config(text=" Button is Clicked!!",font=("Arial",12,"italic") )
+
 # Labels
 
 my_label = Label(text="Sample Text")
 my_label.pack()
 
 #button 
-my_button = Button(text="Click Me!")
+my_button = Button(text="Click Me!",command=click_button)
 my_button.pack()
 
 #entries
@@ -24,10 +28,14 @@ my_entry.pack()
 my_text = Text(height=5, width=10)
 my_text.focus()
 my_text.insert(END,"Example of multi line text entry")
-
 my_text.pack()
 
+# scroll
+def spinbox_used():
+    print(my_spin.get())
 
+my_spin= Spinbox(from_=0,to=10, width=10,command=spinbox_used)
+my_spin.pack()
 
 
 

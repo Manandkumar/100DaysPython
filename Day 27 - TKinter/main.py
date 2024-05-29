2e15a62189a6727938f1d0bc325185a0575a50fd
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-29 21:18:47 +0530

    new

--- Day 27 - TKinter/main.py
+++ Day 27 - TKinter/main.py
@@ -1,45 +1,33 @@
 from tkinter import *
 
 window = Tk()
-window.title("My First GUI")
+window.title("My GIRD")
 window.minsize(width=600, height=400)
 
 #components inside the window
 
+#button funciton 
+def button_click():
+    mylabel= "Button Clicked"
+
 #label 
-mylabel= Label( text ="I'm a label ", font=("Arial", 24))
-mylabel.config(text="New Text")
+mylabel= Label( text ="I'm a label ", font=("Arial", 12))
 mylabel.grid(column=0,row=0)
 
-# mylabel.pack(side="left")
+# Button 
+mybutton= Button(text="New Button", command=button_click)
+mybutton.grid(column=2,row=0)
+# Button 
+mybutton2= Button(text="New Button2", command=button_click)
+mybutton2.grid(column=1,row=1)
 
-# mylabel["text"] = "NEw Name -Anand"
-
-#button 
-
-def button_click():
-    mylabel["text"]= "Button is clicked"
-    mylabel.config(text="Button is clicked!")
-    entry_text=input.get()
-    mylabel.config(text=entry_text)
-    print("you pressed me ")
+#Entry
+myEntry = Entry(width=15)
+myEntry.grid(column=3,row=2)
 
 
-button = Button(text= "Press",command=button_click)
-button.pack()
-
-#entry
-
-input = Entry(width=10)
-input.pack()
-
-
-
-
-
-
-
-
+Label( text ="I'm a label ", font=("Arial", 12))
+mylabel.grid(column=0,row=0)
 
 
 
---
+++ Day 27 - TKinter/main3.py
@@ -0,0 +1,50 @@
+from tkinter import *
+
+window = Tk()
+window.title("My First GUI")
+window.minsize(width=600, height=400)
+
+#components inside the window
+
+#label 
+mylabel= Label( text ="I'm a label ", font=("Arial", 24))
+mylabel.config(text="New Text")
+mylabel.grid(column=0,row=0)
+
+# mylabel.pack(side="left")
+
+# mylabel["text"] = "NEw Name -Anand"
+
+#button 
+
+def button_click():
+    mylabel["text"]= "Button is clicked"
+    mylabel.config(text="Button is clicked!")
+    entry_text=input.get()
+    mylabel.config(text=entry_text)
+    print("you pressed me ")
+
+
+button = Button(text= "Press",command=button_click)
+button.pack()
+
+#entry
+
+input = Entry(width=10)
+# input.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()

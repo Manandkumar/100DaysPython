e3223c7b8bd235d61a602d5778e7cddfc4541b37
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-05-31 17:29:14 +0530

    Update main.py

--- Day 28/main.py
+++ Day 28/main.py
@@ -5,12 +5,22 @@ orange = "#d2601a"
 cream = "#fff1e1"
 
 
-
 # Timer Reset
 
-#Timer Mchanism
+
+
+#Timer Mechanism
+
+def start_timer():
+    count_down(5)
 
 # Countdown 
+
+def count_down(count):
+    canvas.itemconfig(timer_text,text=count)
+    if count > 0:
+        window.after(1000,count_down, count-1)
+
 
 # UI Setup 
 
@@ -18,16 +28,19 @@ window = Tk()
 window.title ("Pamodoro")
 window.config(padx=10,pady=10, bg=black)
 
+
+
 title_label = Label(text= "Timer",fg=orange,bg=black,font=("Arial",50))
 title_label.grid(column=1,row=0)
 
 
 canvas = Canvas(width=300, height=300,bg=cream)
-canvas.create_text(150,150, text="00:00", fill="red",font=("Arial",18))
+timer_text=canvas.create_text(150,150, text="00:00", fill="red",font=("Arial",18))
 canvas.grid(column=1,row=1)
 
 
-start_button = Button(text="Start")
+
+start_button = Button(text="Start",command=start_timer)
 start_button.grid(column=0,row=2)
 
 reset_button = Button(text="Reset")

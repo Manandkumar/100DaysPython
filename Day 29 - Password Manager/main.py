980960f341d9390aa06ac11a15b01d8b0a2c3b34
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-06-06 21:27:47 +0530

    new

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -3,9 +3,10 @@ from tkinter import *
 
 window = Tk()
 window.title ("Password Manager")
+window.config(padx=40,pady=40)
 
 
-canvas = Canvas(width=200, height=200)
+canvas = Canvas(width=400, height=400)
 logo_img =PhotoImage(file="Day 29 - Password Manager\lock.png")
 canvas.create_image(100,100,image=logo_img)
 canvas.pack()

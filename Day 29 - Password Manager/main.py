ecf6ea37a6d6d3164fadeb12a37e329e6719f6ba
Manandkumar <34887573+Manandkumar@users.noreply.github.com>
2024-06-06 21:36:01 +0530

    Update main.py

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,6 +1,8 @@
 
 from tkinter import *
 
+
+#-------------- UI SETUP----------------------
 window = Tk()
 window.title ("Password Manager")
 window.config(padx=40,pady=40)
@@ -9,9 +11,15 @@ window.config(padx=40,pady=40)
 canvas = Canvas(width=400, height=400)
 logo_img =PhotoImage(file="Day 29 - Password Manager\lock.png")
 canvas.create_image(100,100,image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
-
+#Labels 
+website_label = Label(text="Website")
+website_label.grid(row=1,column=0)
+email_label=Label(text="Email / Username")
+email_label.grid(row=2,column=0)
+password_label=Label(text="Password:")
+password_label.grid(row=3,column=0)
 
 
 window.mainloop()
